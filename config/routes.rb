6ca0447b8dc8c140541316ea0a3a2ff12f5595e4
Natsumi82 <122456886+Natsumi82@users.noreply.github.com>
2023-02-01 14:54:25 +0000

    [Add]top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
 end
